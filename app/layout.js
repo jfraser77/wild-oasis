249fd5f065945ed58ae5de3c352540237cc14260
jfraser77 <joefraser7@gmail.com>
2025-03-10 12:25:05 -0500

    first commit

---
+++ app/layout.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Navigation from "./components/Navigation";
+import Logo from "./components/Logo";
+
+export const metadata = {
+  title: "The Wild Oasis",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
+
+        <main>{children}</main>
+        <footer>Copyright by The Wild Oasis</footer>
+      </body>
+    </html>
+  );
+}
