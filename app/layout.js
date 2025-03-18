d9d2b449f2af1afdc2d6d1a902d00aede6dc93d2
Joe Fraser <joefraser7@gmail.com>
2025-03-18 12:29:41 -0500

    Styling Tailwind

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import React from "react";
-import Navigation from "./components/Navigation";
-import Logo from "./components/Logo";
+import Navigation from "@/app/_components/Navigation";
+import Logo from "@/app/_components/Logo";
+
+import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: "The Wild Oasis",
@@ -9,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
